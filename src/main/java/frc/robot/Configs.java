64511d8fef05d59173b401556a8a7f5db7db17a6
Main-Robotics-Lapotop <topsailroboticsteam@gmail.com>
2026-01-29 17:13:28 -0500

    aiming code added

--- src/main/java/frc/robot/Configs.java
+++ src/main/java/frc/robot/Configs.java
@@ -27,6 +27,7 @@ public final class Configs {
             drivingConfig.closedLoop
                     .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
                     // These are example gains you may need to them for your own robot!
+                    //TODO
                     .pid(0.04, 0, 0)
                     .velocityFF(drivingVelocityFeedForward)
                     .outputRange(-1, 1);
