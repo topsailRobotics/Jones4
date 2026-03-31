afa9550c5029478a68d9170c8126484e36a73ac6
Main-Robotics-Lapotop <topsailroboticsteam@gmail.com>
2026-03-31 15:15:35 -0400

    changed driving max speed constant -W

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -31,7 +31,7 @@ public final class Constants {
   public static final class DriveConstants {
     // Driving Parameters - Note that these are not the maximum capable speeds of
     // the robot, rather the allowed maximum speeds
-    public static final double kMaxSpeedMetersPerSecond = 7.60; //7.60 based on documents but need to be sure - Willem 
+    public static final double kMaxSpeedMetersPerSecond = 6.01; //7.60 based on documents but need to be sure - Willem 
     // changed it to 24.93 (feet)
     public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second was 8 pi for comp
 
