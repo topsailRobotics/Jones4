71e7e2ff814b0cd2b37bdcd81f13340a7b1e7463
Main-Robotics-Lapotop <topsailroboticsteam@gmail.com>
2026-04-04 09:50:44 -0400

    removed left over indexer actions in intake command -James

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -92,7 +92,7 @@ public class RobotContainer {
 NamedCommands.registerCommand("ShootLow", new Shoot(m_shooter,1));
     NamedCommands.registerCommand("Shoot Medium", new RunCommand( () -> m_shooter.shooterMediumLow(), m_shooter));// only run shooter
     NamedCommands.registerCommand("Shoot High", new Shoot(m_shooter,4));
-    NamedCommands.registerCommand("Intake", new Intake(m_intake,m_indexer, false));
+    NamedCommands.registerCommand("Intake", new Intake(m_intake, false));
     NamedCommands.registerCommand("Super Charge", new RunCommand( ()->m_intake.superCharge(), m_intake));
     NamedCommands.registerCommand("Lift Intake", new RunCommand( () -> m_intake.intakeUp(0.15), m_intake));
     NamedCommands.registerCommand("Run Indexer", new RunCommand( () -> m_indexer.runIndexVert(), m_indexer));
@@ -186,7 +186,7 @@ NamedCommands.registerCommand("ShootLow", new Shoot(m_shooter,1));
           false)));
 
     Trigger rightTrigger = new Trigger(() -> m_driverController0.getRightTriggerAxis() > 0.5);
-    rightTrigger.whileTrue(new Intake(m_intake, m_indexer, true));
+    rightTrigger.whileTrue(new Intake(m_intake, true));
 
     Trigger leftTrigger = new Trigger(() -> m_driverController0.getLeftTriggerAxis() > 0.5);
 
@@ -206,7 +206,7 @@ NamedCommands.registerCommand("ShootLow", new Shoot(m_shooter,1));
               m_robotDrive));
 
     m_driverController0.rightBumper()
-    .toggleOnTrue(new Intake(m_intake,m_indexer, false)); 
+    .toggleOnTrue(new Intake(m_intake, false)); 
    m_driverController0.x().whileTrue(new RunCommand(
       () -> m_robotDrive.drive(
           -MathUtil.applyDeadband(m_driverController0.getLeftY(), OIConstants.kDriveDeadband),
